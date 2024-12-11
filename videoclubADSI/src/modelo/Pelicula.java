f30d9315d216461c0eedaca79424b178940f1da4
Mikel-Herranz <herranzmikel@gmail.com>
2024-12-11 08:39:01 +0100

    empezando

--- videoclubADSI/src/modelo/Pelicula.java
+++ videoclubADSI/src/modelo/Pelicula.java
@@ -1,6 +1,32 @@
  package modelo;
 
+import java.awt.Image;
+
 public class Pelicula {
     
+    private int codPelicula;
+    private String desc;
+    private Image trailer;
     
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 }
