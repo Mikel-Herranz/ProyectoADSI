dcc3aed12e4625fc283eb260cca5ff2575b1c71a
Mikel-Herranz <herranzmikel@gmail.com>
2025-01-18 13:24:25 +0100

    terminada vista datospersonales y eliminado en modelo clase repetida

--- videoclubADSI/src/vista/CambiarDatosUsu.java
+++ videoclubADSI/src/vista/CambiarDatosUsu.java
@@ -10,11 +10,18 @@ import java.awt.BorderLayout;
 import javax.swing.JLabel;
 import java.awt.Font;
 import javax.swing.SwingConstants;
+import javax.swing.JTextField;
+import javax.swing.JButton;
+import java.awt.Color;
 
 public class CambiarDatosUsu extends JFrame {
 
 	private static final long serialVersionUID = 1L;
 	private JPanel contentPane;
+	private JTextField textField;
+	private JTextField textField_1;
+	private JTextField textField_3;
+	private JTextField textField_4;
 
 	/**
 	 * Launch the application.
@@ -37,7 +44,7 @@ public class CambiarDatosUsu extends JFrame {
 	 */
 	public CambiarDatosUsu() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 666, 664);
+		setBounds(100, 100, 764, 675);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 
@@ -48,6 +55,57 @@ public class CambiarDatosUsu extends JFrame {
 		lblNewLabel.setHorizontalAlignment(SwingConstants.CENTER);
 		lblNewLabel.setFont(new Font("Tahoma", Font.PLAIN, 27));
 		contentPane.add(lblNewLabel, BorderLayout.NORTH);
+		
+		JPanel panel = new JPanel();
+		contentPane.add(panel, BorderLayout.CENTER);
+		panel.setLayout(null);
+		
+		JLabel lblNewLabel_1 = new JLabel("Nombre:");
+		lblNewLabel_1.setHorizontalAlignment(SwingConstants.TRAILING);
+		lblNewLabel_1.setBounds(263, 62, 65, 14);
+		panel.add(lblNewLabel_1);
+		
+		textField = new JTextField();
+		textField.setColumns(10);
+		textField.setBounds(338, 59, 183, 20);
+		panel.add(textField);
+		
+		textField_1 = new JTextField();
+		textField_1.setColumns(10);
+		textField_1.setBounds(338, 135, 183, 20);
+		panel.add(textField_1);
+		
+		JLabel lblNewLabel_2 = new JLabel("Apellido:");
+		lblNewLabel_2.setHorizontalAlignment(SwingConstants.TRAILING);
+		lblNewLabel_2.setBounds(241, 138, 89, 14);
+		panel.add(lblNewLabel_2);
+		
+		textField_3 = new JTextField();
+		textField_3.setColumns(10);
+		textField_3.setBounds(346, 224, 177, 20);
+		panel.add(textField_3);
+		
+		JLabel lblNewLabel_2_2 = new JLabel("Telefono:");
+		lblNewLabel_2_2.setHorizontalAlignment(SwingConstants.TRAILING);
+		lblNewLabel_2_2.setBounds(237, 227, 101, 14);
+		panel.add(lblNewLabel_2_2);
+		
+		JLabel lblNewLabel_2_3 = new JLabel("Fecha de Nacimiento:");
+		lblNewLabel_2_3.setHorizontalAlignment(SwingConstants.TRAILING);
+		lblNewLabel_2_3.setBounds(175, 319, 163, 14);
+		panel.add(lblNewLabel_2_3);
+		
+		textField_4 = new JTextField();
+		textField_4.setColumns(10);
+		textField_4.setBounds(346, 316, 183, 20);
+		panel.add(textField_4);
+		
+		JButton btnModificarDatos = new JButton("Modificar Datos");
+		btnModificarDatos.setForeground(Color.WHITE);
+		btnModificarDatos.setFont(new Font("Tahoma", Font.BOLD, 20));
+		btnModificarDatos.setBackground(new Color(0, 128, 192));
+		btnModificarDatos.setBounds(237, 425, 368, 98);
+		panel.add(btnModificarDatos);
 	}
 
 }
