98f3a3cb198dd2ad620d5ac70aaddb65343d360f
Mikel-Herranz <herranzmikel@gmail.com>
2025-01-18 13:11:15 +0100

    empezado a hacer vista cambiodatos

--- videoclubADSI/src/vista/CambiarDatosUsu.java
+++ videoclubADSI/src/vista/CambiarDatosUsu.java
@@ -5,6 +5,11 @@ import java.awt.EventQueue;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import java.awt.FlowLayout;
+import java.awt.BorderLayout;
+import javax.swing.JLabel;
+import java.awt.Font;
+import javax.swing.SwingConstants;
 
 public class CambiarDatosUsu extends JFrame {
 
@@ -32,11 +37,17 @@ public class CambiarDatosUsu extends JFrame {
 	 */
 	public CambiarDatosUsu() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 450, 300);
+		setBounds(100, 100, 666, 664);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 
 		setContentPane(contentPane);
+		contentPane.setLayout(new BorderLayout(0, 0));
+		
+		JLabel lblNewLabel = new JLabel("Editando Datos Personales");
+		lblNewLabel.setHorizontalAlignment(SwingConstants.CENTER);
+		lblNewLabel.setFont(new Font("Tahoma", Font.PLAIN, 27));
+		contentPane.add(lblNewLabel, BorderLayout.NORTH);
 	}
 
 }
